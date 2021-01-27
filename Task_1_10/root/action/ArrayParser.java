5b274cc4f81b93ea76a03ba4821ca28053a2ddac
lexo4kaa <alesha.krupenko@gmail.com>
2021-01-27 20:12:47 +0300

    adding tests and classes with IntStream

---
+++ Task_1_10/root/action/ArrayParser.java
@@ -0,0 +1,35 @@
+package root.action;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import root.entity.CustomerArray;
+import root.exception.ArrayException;
+
+import java.util.List;
+
+public class ArrayParser {
+    static Logger logger = LogManager.getLogger();
+    public static CustomerArray arrayParser(List<String> arrayList) throws ArrayException {
+        final String SEPARATORS = "[ ,;@]";
+        for(String line : arrayList) {
+            String[] splitLine = line.split(SEPARATORS);
+            int[] resultArray = new int[splitLine.length];
+            boolean flag = true;
+            for(int i = 0; i < splitLine.length; i++) {
+                String current = splitLine[i];
+                try {
+                    int num = Integer.parseInt(current);
+                    resultArray[i] = num;
+                } catch (NumberFormatException e) {
+                    logger.error(current + " at position " + i + " is not an integer number");
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag) {
+                return new CustomerArray(resultArray);
+            }
+        }
+        return new CustomerArray(1);
+    }
+}
