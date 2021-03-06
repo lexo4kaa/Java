5d507a99b9104c5bedfe51ed1f7046d96f6c3d39
lexo4kaa <alesha.krupenko@gmail.com>
2021-03-06 23:35:43 +0300

    finish action, left tests

--- 4_CompositeChain_Krupenko/src/main/java/com/company/composite_chain/action/TextAction.java
+++ 4_CompositeChain_Krupenko/src/main/java/com/company/composite_chain/action/TextAction.java
@@ -4,18 +4,18 @@ import com.company.composite_chain.composite.TextComponent;
 import com.company.composite_chain.composite.TextComposite;
 import com.company.composite_chain.composite.TextType;
 import com.company.composite_chain.exception.CustomerException;
-import com.company.composite_chain.parser.impl.TextParser;
-import com.company.composite_chain.reader.CustomerFileReader;
 
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.HashMap;
 
 import static com.company.composite_chain.reader.CustomerFileReader.logger;
 
 public class TextAction {
     public TextComponent sortParagraphsBySentences(TextComponent text) throws CustomerException {
         if (text == null || text.getTextType() != TextType.TEXT) {
-            logger.error("text is null or not text", text);
+            logger.error("text is null or not text: " + text);
             throw new CustomerException("text is null or not text: " + text);
         }
         List<TextComponent> paragraphs = text.getAll();
@@ -31,8 +31,8 @@ public class TextAction {
             paragraphs.set(j, current);
         }
         TextComponent resultText = new TextComposite(TextType.TEXT);
-        for (int k = 0; k < length; k++) {
-            resultText.add(paragraphs.get(k));
+        for (TextComponent paragraph : paragraphs) {
+            resultText.add(paragraph);
         }
         return resultText;
     }
@@ -41,22 +41,93 @@ public class TextAction {
         return Integer.compare(component1.getAll().size(), component2.getAll().size());
     }
 
-    public List<TextComponent> getSentencesWithLongestWord(TextComposite text) {
-        return null;
+    public List<TextComponent> getSentencesWithLongestWord(TextComponent text) throws CustomerException {
+        if (text == null || text.getTextType() != TextType.TEXT) {
+            logger.error("text is null or not text: " + text);
+            throw new CustomerException("text is null or not text: " + text);
+        }
+        int maxLength = findMaxLengthOfWords(text);
+        List<TextComponent> listWithLongestWords = new ArrayList<>();
+        for (TextComponent paragraph : text.getAll()) {
+            for (TextComponent sentence : paragraph.getAll()) {
+                for (TextComponent lexeme : sentence.getAll()) {
+                    for (TextComponent word : lexeme.getAll()) {
+                        int wordLength = 0;
+                        for (TextComponent symbol : word.getAll()) {
+                            wordLength++;
+                        }
+                        if (wordLength == maxLength && !listWithLongestWords.contains(sentence)) {
+                            listWithLongestWords.add(sentence);
+                        }
+                    }
+                }
+            }
+        }
+        return listWithLongestWords;
     }
 
-    public void deleteSentencesWithWordsLessNumber(TextComposite text, int value) {
-
+    public int findMaxLengthOfWords(TextComponent text) throws CustomerException {
+        if (text == null || text.getTextType() != TextType.TEXT) {
+            logger.error("text is null or not text: " + text);
+            throw new CustomerException("text is null or not text: " + text);
+        }
+        int maxLength = 1;
+        for (TextComponent paragraph : text.getAll()) {
+            for (TextComponent sentence : paragraph.getAll()) {
+                for (TextComponent lexeme : sentence.getAll()) {
+                    for (TextComponent word : lexeme.getAll()) {
+                        int wordLength = 0;
+                        for (TextComponent symbol : word.getAll()) {
+                            wordLength++;
+                        }
+                        if (wordLength > maxLength) {
+                            maxLength = wordLength;
+                        }
+                    }
+                }
+            }
+        }
+        return maxLength;
     }
 
-    public Map<String, Integer> findWordsAndCountThem(TextComposite text) {
-        return null;
+    public TextComponent deleteSentencesWithWordsLessThanNumber(TextComponent text, int value) throws CustomerException {
+        if (text == null || text.getTextType() != TextType.TEXT) {
+            logger.error("text is null or not text: " + text);
+            throw new CustomerException("text is null or not text: " + text);
+        }
+        for (TextComponent paragraph : text.getAll()) {
+            List<TextComponent> sentences = paragraph.getAll();
+            for (int i = 0; i < sentences.size(); i++) {
+                TextComponent sentence = sentences.get(i);
+                if (sentence.getAll().size() < value) {
+                    paragraph.remove(sentence);
+                }
+            }
+        }
+        return text;
     }
 
-    public static void main(String[] args) throws CustomerException {
-        TextAction action = new TextAction();
-        TextParser parser = TextParser.getInstance();
-        System.out.println(action.sortParagraphsBySentences(
-                parser.parse(CustomerFileReader.fileReading("src/main/resources/text.txt"))).toString());
+    public Map<String, Integer> findWordsAndTheirCount(TextComponent text) throws CustomerException {
+        if (text == null || text.getTextType() != TextType.TEXT) {
+            logger.error("text is null or not text: " + text);
+            throw new CustomerException("text is null or not text: " + text);
+        }
+        Map<String, Integer> wordsAndTheirCount = new HashMap<>();
+        for (TextComponent paragraph : text.getAll()) {
+            for (TextComponent sentence : paragraph.getAll()) {
+                for (TextComponent lexeme : sentence.getAll()) {
+                    for (TextComponent word : lexeme.getAll()) {
+                        String lowerCaseWord = word.toString().toLowerCase();
+                        if(wordsAndTheirCount.containsKey(lowerCaseWord)) {
+                            int count = wordsAndTheirCount.get(lowerCaseWord);
+                            wordsAndTheirCount.put(lowerCaseWord, count + 1);
+                        } else {
+                            wordsAndTheirCount.put(lowerCaseWord, 1);
+                        }
+                    }
+                }
+            }
+        }
+        return wordsAndTheirCount;
     }
 }
---
+++ 4_CompositeChain_Krupenko/src/test/java/com/company/composite_chain/action/TextActionTest.java
@@ -0,0 +1,20 @@
+package com.company.composite_chain.action;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+
+public class TextActionTest {
+
+//    @BeforeMethod
+    public void setUp() {
+    }
+
+  //  @AfterMethod
+    public void tearDown() {
+    }
+}
